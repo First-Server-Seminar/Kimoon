7282a0ee476254298b4007133de139510b2430a1
kkm97351@gmail.com <kkm97351@gmail.com>
2020-10-10 17:13:23 +0900

    [refactor] : 디렉토리 구조 변경

---
+++ 1st-seminar/null_undefined.js
@@ -0,0 +1,9 @@
+let nothing = null;
+console.log(`nothing : ${nothing}, type: ${typeof nothing}`); //object, 설계상 오류
+let x;
+console.log(`x: ${x}, type ${typeof x}`); //undefined
+
+// null vs undefined
+console.log('null vs undefined');
+console.log('null === undefined: ', null === undefined);
+console.log('null == undefined: ', null == undefined);
