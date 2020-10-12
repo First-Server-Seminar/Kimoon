e41ea78755cd35d80ed1ced654029c4b2bfb3d75
tributetothemoon <kkm97351@gmail.com>
2020-10-13 02:36:04 +0900

    feat : add printMembers functions for homework1 level2 homework/homework1/ourMember.js has been modified

--- homework/homework1/ourMember.js
+++ homework/homework1/ourMember.js
@@ -1,4 +1,4 @@
-const member = [
+const members = [
     {
         "name" : "김기문",
         "address" : "서울특별시 동작구 상도동",
@@ -16,5 +16,22 @@ const member = [
         "address" : "서울특별시 중랑구",
         "age" : 23,
         "hobby" : "게임하기, 맛있는거 먹기, 돌아다니기"
+    },
+    {
+        "name" : "김채원",
+        "address" : "서울특별시 양천구",
+        "age" : 22,
+        "hobby" : "자전거타기, 강아지 영상보기, 염색하기"
     }
 ]
+
+const printMembers = (members) => {
+    members.forEach(function(element){
+        console.log(`이름 : ${element.name}`);
+        console.log(`주소 : ${element.address}`);
+        console.log(`나이 : ${element.age}`);
+        console.log(`취미 : ${element.hobby}`);
+        console.log();
+    })
+}
+printMembers(members);
