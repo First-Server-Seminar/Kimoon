12af8d366c3b6f2163ad4d87a04d98f8fb264daf
tributetothemoon <kkm97351@gmail.com>
2020-11-25 06:44:34 +0900

    feat: 2nd-seminar Homework Level3

---
+++ 2nd-seminar/encrypt.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const crypto = require('crypto');
+
+async function encrypt(fileName, password){
+    crypto.randomBytes(64, (err, buf)=>{
+        const salt = buf.toString('base64');
+        crypto.pbkdf2('비밀번호', salt, 100000, 64, 'sha512', (err, buf) => {
+            fs.writeFile(fileName, buf.toString('base64'), () => {
+                console.log(`${fileName} has been created.`);
+            })
+        });
+    });
+}
+
+const fileName = "result.txt";
+const password = "testpassword";
+
+encrypt(fileName, password);
