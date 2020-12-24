5056a31c58fe1dfea992d7b6ccb0dad05ec820f9
tributetothemoon <kkm97351@gmail.com>
2020-12-25 07:39:26 +0900

    fix: Week5 Level 1 modify

--- 5th-seminar/controller/multerController.js
+++ 5th-seminar/controller/multerController.js
@@ -1,22 +1,30 @@
+const util = require('../modules/util');
+const responseMessage = require('../modules/responseMessage');
+const statusCode = require('../modules/statusCode');
+const { Post } = require('../models');
+
 module.exports = {
   single: async (req, res) => {
-    const image = req.file.location;
+    const { title, contents } = req.body;
+    const imageUrl = req.file.location;
+
+    if (!title || !contents || !imageUrl) {
+      return res.status(statusCode.BAD_REQUEST).send(util.success(statusCode.BAD_REQUEST, responseMessage.CREATE_POST_FAIL));
+    }
     console.log(req.file);
     console.log(req.body);
-    res.send({
-      imageUrl: image,
-      file: req.file,
-      body: req.body
-    });
-  },
-  array: async (req, res) => {
-    const imageUrls = req.files.map(file => file.location);
-    console.log(req.files);
-    console.log(req.body);
-    res.send({
-      imageUrls: imageUrls,
-      file: req.files,
-      body: req.body
-    });
+    
+    try {
+      const post = await Post.create({
+        title,
+        contents,
+        postImageUrl: imageUrl
+      });
+  
+      return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.CREATE_POST_SUCCESS, post));
+    } catch (error) {
+      console.log(error);
+      return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.CREATE_POST_FAIL));
+    }
   }
 }
--- 5th-seminar/routes/multer/index.js
+++ 5th-seminar/routes/multer/index.js
@@ -5,6 +5,15 @@ const upload = require('../../modules/multer');
 
 router.post('/single', upload.single('image'), multerController.single);
 
-router.post('/array', upload.array('images', 3), multerController.array);
+router.post('/array', upload.array('images', 3), async (req, res) => {
+  const imageUrls = req.files.map(file => file.location);
+  console.log(req.files);
+  console.log(req.body);
+  res.send({
+    imageUrls: imageUrls,
+    file: req.files,
+    body: req.body
+  });
+});
 
 module.exports = router;
